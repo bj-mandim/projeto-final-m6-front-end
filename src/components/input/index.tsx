749c46e440f33d023ca2c2d393ac0fe5e8fb53b4
AmandaIsMe-alt <amanda.costa1301@hotmail.com>
2023-04-12 12:49:26 -0300

    fixing inputs

---
+++ src/components/input/index.tsx
@@ -0,0 +1,18 @@
+
+import { InputContainer } from "./style";
+
+const Input = (
+) => {
+ 
+  return (
+
+    <InputContainer>
+
+      <label></label>
+      <input />
+
+    </InputContainer>
+  );
+}
+
+export default Input;
