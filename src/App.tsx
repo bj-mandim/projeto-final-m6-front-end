b310b94c9507a07cf47f3f5c803176b3ba68ab26
Gabriel Werneck <gabrielwrnck@gmail.com>
2023-04-12 17:11:01 -0300

    debbuging getCards

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Home from "./pages/Home";
 import GlobalStyle from "./global/style";
 // import Card from "./components/card";
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="" element={<Home />} />
       </Routes>
     </>
   );
