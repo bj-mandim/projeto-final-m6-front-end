2641d5cfe11101149330e4150c8338415612854c
bj-mandim <beatrizjuliomandim@gmail.com>
2023-04-13 12:38:17 -0300

    fix: home filter

--- src/components/button/style.ts
+++ src/components/button/style.ts
@@ -1,12 +1,12 @@
 import styled, { css } from "styled-components";
 
 interface iButtonProps {
-medium?: boolean;
-model?: string;
-largura?:number;
+  medium?: boolean;
+  model?: string;
+  widths?: number;
 }
 
-export const Button = styled.button<iButtonProps>`
+const Button = styled.button<iButtonProps>`
   outline: none;
   border: 1px solid;
   border-radius: 4px;
@@ -17,34 +17,33 @@ export const Button = styled.button<iButtonProps>`
   cursor: pointer;
   text-decoration: none;
   transition: all 300ms ease-in-out;
-  width: ${({largura})=>largura?`${largura}%`:'fit-content'};
+  width: ${({ widths }) => (widths ? `${widths}%` : "fit-content")};
 
   ${({ medium }) =>
     !medium
-      ?css`
-        padding: 24px 28px;
-        font-size: var(--button-big-text-size);
-      `
-      :css`
-        padding: 12px 20px;
-        font-size: var(--button-medium-text-size);
-      `}
+      ? css`
+          padding: 24px 28px;
+          font-size: var(--button-big-text-size);
+        `
+      : css`
+          padding: 12px 20px;
+          font-size: var(--button-medium-text-size);
+        `}
 
-  ${({model})=>{
-    switch ( model?.toLowerCase()){
-
-      case 'model-1':
+  ${({ model }) => {
+    switch (model?.toLowerCase()) {
+      case "model-1":
         return css`
           background: var(--grey0);
           border-color: var(--grey0);
-          color: #FFFFFF;
+          color: #ffffff;
 
           &:hover {
             background: var(--grey1);
             border-color: var(--grey1);
           }
-        `
-      case 'model-2':
+        `;
+      case "model-2":
         return css`
           background: var(--grey6);
           border-color: var(--grey6);
@@ -53,130 +52,140 @@ export const Button = styled.button<iButtonProps>`
             background: var(--grey5);
             border-color: var(--grey5);
           }
-        `
+        `;
 
-      case 'model-3':
+      case "max_min_filter":
         return css`
           background: var(--grey5);
           border-color: var(--grey5);
-          color: #FFFFFF;
+          color: var(--grey2);
 
           &:hover {
             background: var(--brand1);
             border: 1px solid var(--brand1);
           }
-        `
+        `;
 
-      case 'model-4':
+      case "model-4":
         return css`
           background: var(--brand2);
           border-color: var(--brand2);
-          color: #FFFFFF;
+          color: #ffffff;
 
           &:hover {
             background: var(--brand4);
             border-color: var(--brand4);
             color: var(--brand1);
           }
-        `
-            
-      case 'model-5':
+        `;
+
+      case "model-5":
         return css`
           background: var(--grey10);
           border-color: var(--grey10);
-        `
+        `;
 
-      case 'model-6':
+      case "model-6":
         return css`
           background: var(--grey10);
           border-color: var(--grey1);
-            
+
           &:hover {
             background: var(--grey1);
             border-color: var(--grey1);
-            color: #FFFFFF;
+            color: #ffffff;
           }
-        `
+        `;
 
-      case 'model-7':
+      case "model-7":
         return css`
           background: var(--grey10);
           border-color: var(--grey4);
           color: var(--grey0);
-            
+
           &:hover {
             background: var(--grey1);
-            border-color:var(--grey1);
-            color: #FFFFFF;
+            border-color: var(--grey1);
+            color: #ffffff;
           }
-        `
+        `;
 
-      case 'model-8':
+      case "model-8":
         return css`
           background: var(--grey10);
-          border-color:var(--brand1);
+          border-color: var(--brand1);
           color: var(--brand1);
-            
+
           &:hover {
             background: var(--brand4);
-            border-color:var(--brand1);
+            border-color: var(--brand1);
             color: var(--brand1);
           }
-        `
+        `;
 
-      case 'model-9':
+      case "model-9":
         return css`
           background: var(--grey10);
-          border-color:var(--grey10);
+          border-color: var(--grey10);
           color: var(--grey0);
-            
+
           &:hover {
             background: var(--grey8);
-            border-color:var(--grey8);
+            border-color: var(--grey8);
           }
-        `
+        `;
 
-      case 'model-10':
+      case "model-10":
         return css`
           background: var(--alert3);
-          border-color:var(--alert3);
+          border-color: var(--alert3);
           color: var(--alert1);
-            
+
           &:hover {
             background: var(--alert2);
-            border-color:var(--alert2);
+            border-color: var(--alert2);
           }
-        `
+        `;
 
-      case 'model-11':
+      case "model-11":
         return css`
           background: var(--sucess3);
-          border-color:var(--sucess3);
+          border-color: var(--sucess3);
           color: var(--sucess1);
-            
+
           &:hover {
             background: var(--sucess2);
-            border-color:var(--sucess2);
+            border-color: var(--sucess2);
           }
-        `
-      case 'model-12':
+        `;
+
+      case "option-filter":
+        return css`
+          background: var(--gray6);
+          border-color: transparent;
+          color: var(--grey3);
+          text-align: start;
+        `;
+      case "model-12":
         return css`
           background: var(--brand3);
-          border-color:var(--brand3);
-          color: #FFFFFF;
-        `
+          border-color: var(--brand3);
+          color: #ffffff;
+        `;
 
       default:
         return css`
           background: var(--grey0);
           border-color: var(--grey0);
-          color: #FFFFFF;
+          color: #ffffff;
 
           &:hover {
             background: var(--grey1);
             border-color: var(--grey1);
           }
-        `
+        `;
     }
-  }
-}`;
+  }}
+`;
+
+export default Button;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,9 @@
 import Header from "../../components/header";
 import Card from "./renderCards";
 import { useState } from "react";
+import "./style.css";
+import Button from "../../components/button/style";
+import car from "../../img/Car.png";
 
 function Home() {
   const [getCars, setGetCars] = useState([
@@ -58,7 +61,129 @@ function Home() {
   return (
     <>
       <Header></Header>
-      <Card getCars={getCars} />
+      <main className="main_home">
+        <section className="container">
+          <div>
+            <img src={car} alt="car" />
+            <div>
+              <h1 className="heading-1">Motors Shop</h1>
+              <p className="heading-3">
+                A melhor plataforma de anúncios de carros do país
+              </p>
+            </div>
+          </div>
+        </section>
+        <div className="container_filter_cards">
+          <section className="filter_card">
+            <div className="filter">
+              <div className="container_filter">
+                <h3>Marca</h3>
+                <Button model="option-filter" widths={50} medium>
+                  Porsche
+                </Button>
+              </div>
+              <div className="container_filter">
+                <h3>Modelo</h3>
+                <Button model="option-filter" widths={60} medium>
+                  Porsche 718
+                </Button>
+              </div>
+              <div className="container_filter">
+                <h3>Cor</h3>
+                <Button model="option-filter" widths={50} medium>
+                  Azul
+                </Button>
+                <Button model="option-filter" widths={50} medium>
+                  Branca
+                </Button>
+                <Button model="option-filter" widths={50} medium>
+                  Cinza
+                </Button>
+                <Button model="option-filter" widths={50} medium>
+                  Prata
+                </Button>
+                <Button model="option-filter" widths={50} medium>
+                  Preta
+                </Button>
+                <Button model="option-filter" widths={50} medium>
+                  Verde
+                </Button>
+              </div>
+              <div className="container_filter">
+                <h3>Ano</h3>
+                <Button model="option-filter" widths={50} medium>
+                  2022
+                </Button>
+                <Button model="option-filter" widths={50} medium>
+                  2021
+                </Button>
+                <Button model="option-filter" widths={50} medium>
+                  2018
+                </Button>
+                <Button model="option-filter" widths={50} medium>
+                  2015
+                </Button>
+                <Button model="option-filter" widths={50} medium>
+                  2013
+                </Button>
+                <Button model="option-filter" widths={50} medium>
+                  2012
+                </Button>
+                <Button model="option-filter" widths={50} medium>
+                  2010
+                </Button>
+              </div>
+
+              <div className="container_filter">
+                <h3>Combustível</h3>
+                <Button model="option-filter" widths={50} medium>
+                  Diesel
+                </Button>
+                <Button model="option-filter" widths={50} medium>
+                  Etanol
+                </Button>
+                <Button model="option-filter" widths={50} medium>
+                  Gasolina
+                </Button>
+                <Button model="option-filter" widths={50} medium>
+                  Flex
+                </Button>
+              </div>
+
+              <div className="container_max_min">
+                <h3>Km</h3>
+                <div>
+                  <Button model="max_min_filter" widths={45} medium>
+                    Mínina
+                  </Button>
+                  <Button model="max_min_filter" widths={45} medium>
+                    Máxima
+                  </Button>
+                </div>
+              </div>
+
+              <div className="container_max_min">
+                <h3>Preço</h3>
+                <div>
+                  <Button model="max_min_filter" widths={45} medium>
+                    Mínino
+                  </Button>
+                  <Button model="max_min_filter" widths={45} medium>
+                    Máximo
+                  </Button>
+                </div>
+              </div>
+            </div>
+            <div className="button_clear_filter">
+              <Button model="model-4">Limpar Filtros</Button>
+            </div>
+          </section>
+          <section className="render_cards">
+            {/* Elias */}
+            <Card getCars={getCars} />
+          </section>
+        </div>
+      </main>
     </>
   );
 }
