a3cbcd9051146f73d58c2cd3cebcf17cbbe30ada
Elias <e.borges289@gmail.com>
2023-04-13 17:37:42 -0300

    feat:

--- src/pages/Home/renderCards/index.tsx
+++ src/pages/Home/renderCards/index.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable array-callback-return */
 // import { StyledCards } from "./style";
 
+import { StyledCards } from "../../../components/card/style";
+
 interface iCardProps {
   getCars: car[]
 }
@@ -18,6 +20,27 @@ interface car {
 const Card = ({ getCars }: iCardProps) => {
   console.log(getCars);
   return (<>
+  <StyledCards>
+    <div className="boxImage">
+      <img src="../../../img/Car.png" alt="" />
+      <span className="statusCar">Ativo</span>
+    </div>
+    <div className="descriptionCar">
+      <h1>Carrinho Bonito</h1>
+      <p>Description do carrinho bonito só que aqui tem que ser grande pra poder colocar o negocinho para ela sumir caso ela seja muito grande pra não atrapalhar a lindeza que esse card vai ficar</p>
+      <div className="boxUser">
+        <img src="" alt="" />
+        <h2>Elias Borges</h2>
+      </div>
+      <div className="boxKmYearPrice">
+        <div className="kmYear">
+          <span className="KMCar">0 KM</span>
+          <span className="yearCar">2023</span>
+        </div>
+        <span className="priceCar">R$ 100.000,00</span>
+      </div>
+    </div>
+  </StyledCards>
   {
     getCars.map((car)=>{
        return(<>
