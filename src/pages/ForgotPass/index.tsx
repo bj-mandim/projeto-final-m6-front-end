c2ff4b96d179b5d17103a0ea06f560e27970979b
AmandaIsMe-alt <amanda.costa1301@hotmail.com>
2023-04-26 13:52:54 -0300

    fixing password page

--- src/pages/ForgotPass/index.tsx
+++ src/pages/ForgotPass/index.tsx
@@ -9,18 +9,19 @@ function ForgotPassword() {
         <Header></Header>
         <Form>
             <h2 className="header">Esqueceu sua senha?</h2>
-            <p className="body-2">Informe seu e-mail para receber um link de redefinação de senha</p>
     
             <div className="content_login">
-                <label htmlFor="email">Informe seu e-mail</label>
-                <input type="email" id="email" placeholder="Digitar usuário" />
-                <button type="submit" className="btn btn-primary w-100" style={{marginTop: 32}}>Enviar</button>
+                <label htmlFor="email">Informe sua nova senha.</label>
+                <input type="email" id="email" placeholder="Digitar senha" />
+                <label htmlFor="email">Confirme sua nova senha.</label>
+                <input type="email" id="email" placeholder="Digitar senha" />
+                <button type="submit" className="btn btn-primary w-100" style={{marginTop: 32}}>Salvar</button>
             </div>
             
-            <p className="btn btn-medium btn-success" style={{lineHeight: 1}}>E-mail de recuperação de senha enviado com sucesso!</p>
+            <p className="btn btn-medium btn-success" style={{lineHeight: 1}}>Senha redefinida com sucesso!</p>
             
             <br/>
-            <Link to={"/login"} className="extra-text">{'<-'} Voltar a página de login</Link>
+            <Link to={"/login"} className="extra-text">{'<-'} Voltar à página de login</Link>
         </Form>
         <Footer />
       </>
