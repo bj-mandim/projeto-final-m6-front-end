56530e08dacefb2cf5ce191aa1608072515b54ee
bj-mandim <beatrizjuliomandim@gmail.com>
2023-04-26 15:01:39 -0300

    feat and fix: toast 100% and bug mobile 100%

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,3 @@
-/* import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Home from "./pages/Home";
-import GlobalStyle from "./global/style";
-import Product from "./pages/Product";
-
-function App() {
-  return (
-    <>
-      <GlobalStyle />
-
-      <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/product" element={<Product />} />
-      </Routes>
-    </>
-  );
-}
-
-export default App; */
-
 import RoutesComponent from "./routes";
 
 function App() {
