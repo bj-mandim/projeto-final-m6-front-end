26f64763cf7d3813db067db74b1b67210de8a09e
Gabriel Werneck <gabrielwrnck@gmail.com>
2023-05-07 17:55:59 -0300

    Moment on commments

--- src/components/comment/index.tsx
+++ src/components/comment/index.tsx
@@ -2,38 +2,40 @@ import { useNavigate } from "react-router-dom";
 import { Div } from "./style";
 import { ContextApi } from "../../contexts";
 import { useContext } from "react";
+import moment from "moment";
+import "moment/locale/pt-br";
 
 //Necessario arrumar as interfaces do ContextApi
-interface iCardMaybe{
-	card:{
-		comments:{id:string ,user:{name:String}, created_at:String, comment:string}[]
-	}
-
+interface iCardMaybe {
+  card: {
+    comments: {
+      id: string;
+      user: { name: String };
+      created_at: any;
+      comment: string;
+    }[];
+  };
 }
 
 function Comment() {
-
   const navigate = useNavigate();
-  const {card}:iCardMaybe = useContext(ContextApi)
+  const { card }: iCardMaybe = useContext(ContextApi);
   return (
     <>
-		<Div>
-			<h3>Comentários</h3>
-			<ul>
-				{card.comments.map(comment=>
-					<section key={comment.id}>
-	
-							<div className="comment">
-						
-							<h4>{comment.user.name}</h4>
-							<span>Criado em {comment.created_at.split(`T`)[0]}</span>
-							</div>
-							<p>{comment.comment}</p>	
-				
-					</section>
-				)}
-			</ul>
-		</Div>
+      <Div>
+        <h3>Comentários</h3>
+        <ul>
+          {card.comments.map((comment) => (
+            <section key={comment.id}>
+              <div className="comment">
+                <h4>{comment.user.name}</h4>
+                <span>{moment(new Date(comment.created_at)).fromNow()}</span>
+              </div>
+              <p>{comment.comment}</p>
+            </section>
+          ))}
+        </ul>
+      </Div>
     </>
   );
 }
